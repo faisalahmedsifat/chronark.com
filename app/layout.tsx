001888b2d9a38f3d91fe68c0ab5ea4649eaab6b2
Faisal Ahmed Sifat <Faisalahmed531@gmail.com>
2024-09-20 18:13:06 +0600

    feat: fix some names

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "theentrepreneuw",
+    template: "%s | theentrepreneuw",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "theentrepreneuw",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://theentrepreneuw.com",
+    siteName: "theentrepreneuw",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://theentrepreneuw.com/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "The Entrepreneuw",
     card: "summary_large_image",
   },
   icons: {
